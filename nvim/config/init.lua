f68f06af1130fe3870ba549662d23b7a1f8af37b
Pete Burgess <petebur@gmail.com>
2025-06-30 13:07:41 -0600

    Restart nvim config

--- nvim/config/init.lua
+++ nvim/config/init.lua
@@ -1,131 +1,28 @@
--- init.lua for Peter Burgess. Contact petebur@gmail.com
---
--- Global Settings
---
--- Basic Settings
--- Set leader and local leader
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ','
+require("config.lazy")
 
--- Set searches to be case insensitive by default
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.incsearch = true
+vim.g.mapleader = " "
 
--- Display line numbers
+vim.opt.shiftwidth = 4
 vim.opt.number = true
+vim.opt.clipboard = "unnamedplus"
 
--- Minimal number of screen lines to keep above and below the cursor
-vim.opt.scrolloff = 5
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("n", "<leader><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<leader>x", ":.lua<CR>")
+vim.keymap.set("v", "<leader>x", ":lua<CR>")
 
--- Show which line the cursor is on
--- vim.opt.cursorline = true
+-- Keymap for (s)mall (t)erminal at the bottom of the screen
+-- Lookup vim.fn.chansend for sending commands to the terminal
+vim.keymap.set("n", "<leader>st", function()
+  -- Create a new window
+  vim.cmd.new()
+  -- Open terminal in new window
+  vim.cmd.term()
+  -- Send terminal to the very bottom
+  vim.cmd.wincmd("J")
+  -- Set terminal window height to 10 lines
+  vim.api.nvim_win_set_height(0, 10)
+end)
 
--- Easily turn off highlighting
-vim.keymap.set('n', '<leader>n', '<cmd>nohl<cr>', {desc = 'Turn off search result highlight'})
-
--- Navigation remappings
-vim.keymap.set('i', 'jk', '<esc>', {desc = 'Map jk to leave insert mode'})
-vim.keymap.set('v', '<leader>jk', '<esc>', {desc = 'Map leader jk to leave insert mode'})
-
--- Allow for easier navigation around splits
-vim.keymap.set('n', '<leader>h', '<c-w>h', {desc = 'Go left one window with leader h'})
-vim.keymap.set('n', '<leader>j', '<c-w>j', {desc = 'Go down one window with leader j'})
-vim.keymap.set('n', '<leader>k', '<c-w>k', {desc = 'Go up one window with leader k'})
-vim.keymap.set('n', '<leader>l', '<c-w>l', {desc = 'Go right one window with leader l'})
-
--- Open vertical splits on the right
-vim.opt.splitright = true
-
--- Easily open file explorer
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
-
--- More easily go left or right in insert mode
-vim.keymap.set('i', '<c-l>', '<right>')
-vim.keymap.set('i', '<c-h>', '<left>')
-
--- Set default indentation to 2 spaces
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.softtabstop=2
-vim.opt.shiftround = true
-
--- Text mappings
--- Put quotes around current word in normal mode
-vim.keymap.set('n', '<leader>"', 'lBi"<esc>Ea"<esc>')
-vim.keymap.set('n', "<leader>'", "lBi'<esc>Ea'<esc>")
-vim.keymap.set('n', '<leader>`', 'lBi`<esc>Ea`<esc>')
-
--- Put quotes around selection in visual mode
-vim.keymap.set('v', '<leader>"', '<esc>`>a"<esc>`<i"<esc>vf"')
-vim.keymap.set('v', "<leader>'", "<esc>`>a'<esc>`<i'<esc>vf'")
-
--- Create a new empty line then immediately return to normal mode
-vim.keymap.set('n', '<leader>o', 'o<esc>')
-vim.keymap.set('n', '<leader>O', 'O<esc>')
-
--- Movement mappings
--- Allow movements to apply in and around parentheses, curly braces and square brackets
-vim.keymap.set('o', 'in(', '<cmd>normal! f(vi(<cr>')
-vim.keymap.set('o', 'il(', '<cmd>normal! F)vi(<cr>')
-vim.keymap.set('o', 'an(', '<cmd>normal! f(va(<cr>')
-vim.keymap.set('o', 'al(', '<cmd>normal! F)va(<cr>')
-
-vim.keymap.set('o', 'in[', '<cmd>normal! f[vi[<cr>')
-vim.keymap.set('o', 'il[', '<cmd>normal! F]vi[<cr>')
-vim.keymap.set('o', 'an[', '<cmd>normal! f[va[<cr>')
-vim.keymap.set('o', 'al[', '<cmd>normal! F]va[<cr>')
-
-vim.keymap.set('o', 'in{', '<cmd>normal! f{vi{<cr>')
-vim.keymap.set('o', 'il{', '<cmd>normal! F}vi{<cr>')
-vim.keymap.set('o', 'an{', '<cmd>normal! f{va{<cr>')
-vim.keymap.set('o', 'al{', '<cmd>normal! F}va{<cr>')
-
--- Misc mappings
--- Allow easily opening init.lua file from any nvim session and reset the source
-vim.keymap.set('n', '<leader>ev', '<cmd>exe "vsplit ".stdpath("config")."/init.lua"<cr>')
-vim.keymap.set('n', '<leader>hev', '<cmd>exe "split ".stdpath("config")."/init.lua"<cr>')
-vim.keymap.set('n', '<leader>sv', '<cmd>exe "source ".stdpath("config")."/init.lua"<cr>')
-
--- Common abbreviations and useful shortcuts
-
-vim.cmd([[
-  :iabbrev adn and
-  :iabbrev waht what
-  :iabbrev tehn then
-  :iabbrev teh the
-  :iabbrev Teh The
-  :iabbrev @@1 petebur@gmail.com
-  :iabbrev ipd import pandas as pd
-  :iabbrev inp import numpy as np
-]])
-
--- Set the colorscheme
-
-vim.cmd('colorscheme halfdark')
-
--- Python autocommands
-local pycmd = vim.api.nvim_create_augroup('python', { clear = false })
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'python',
-  callback = function()
-    vim.keymap.set('n', '<F5>', ':!python %<CR>')
-  end,
-  desc = 'Allow the current Python file to be run with F5',
-  group = pycmd
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'python',
-  callback = function()
-    vim.treesitter.start()
-  end,
-  group = pycmd,
-  desc = 'Start Treesitter when opening a Python file'
-})
--- Enable treesitter for syntax highlighting
--- vim.treesitter.start()
-
--- Load plugins
--- require('config.lazy')
+-- Get to normal mode in the terminal by double hitting esc
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
--- nvim/config/queries/python/highlights.scm
+++
@@ -1,9 +0,0 @@
-;;extends
-
-; Personalized queries
-((import_from_statement
-  module_name: (dotted_name 
-    (identifier) @module )
-  name: (dotted_name
-          (identifier) @module )
-  ))
